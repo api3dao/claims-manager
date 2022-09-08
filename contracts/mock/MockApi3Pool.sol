86b3d26a6438af9a487b72d0b7d0ba2a81836c46
bbenligiray <bbenligiray@gmail.com>
2022-09-08 19:26:23 +0300

    Have MockApi3Pool totalStake be a constructor argument

--- contracts/mock/MockApi3Pool.sol
+++ contracts/mock/MockApi3Pool.sol
@@ -6,10 +6,11 @@ import "./MockApi3Token.sol";
 /* solhint-disable */
 contract MockApi3Pool {
     address public immutable api3Token;
-    uint256 public constant totalStake = 1_000_000 ether;
+    uint256 public immutable totalStake;
 
-    constructor(address _api3Token) public {
+    constructor(address _api3Token, uint256 _totalStake) public {
         api3Token = _api3Token;
+        totalStake = _totalStake;
     }
 
     function payOutClaim(address recipient, uint256 amount) external {
