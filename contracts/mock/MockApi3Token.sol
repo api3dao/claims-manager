798d584ba56d339baf9c67dc080cc209f50c4371
bbenligiray <bbenligiray@gmail.com>
2022-09-13 09:15:50 +0300

    Remove redundant Ownable

--- contracts/mock/MockApi3Token.sol
+++ contracts/mock/MockApi3Token.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 /* solhint-disable */
-contract MockApi3Token is ERC20, Ownable {
+contract MockApi3Token is ERC20 {
     constructor() public ERC20("API3", "API3") {}
 
     function mint(address account, uint256 amount) external {
