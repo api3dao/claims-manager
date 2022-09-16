d828868d337b73c73911698c432526723f6ea150
bbenligiray <bbenligiray@gmail.com>
2022-09-16 17:22:07 +0300

    Implement QuotaControlled

---
+++ contracts/interfaces/IQuotaControlled.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IQuotaControlled {
+    function getQuotaUsage(address account) external view returns (uint224);
+
+    function accountToAccumulatedQuotaUsageCheckpoints(
+        address account,
+        uint256 checkpointIndex
+    ) external view returns (uint32 fromTimestamp, uint224 value);
+
+    function accountToQuota(address account)
+        external
+        view
+        returns (uint32 period, uint224 amountInApi3);
+}
