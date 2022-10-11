96d699126d1ab2148c66a394a64d9ab1b4d42fc3
bbenligiray <bbenligiray@gmail.com>
2022-10-11 17:03:38 +0300

    Abstract away the policy creation function call parameters

--- contracts/policy-agents/PolicyOfferExecutor.sol
+++ contracts/policy-agents/PolicyOfferExecutor.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.0;
 import "../interfaces/IClaimsManager.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract PolicyOfferExecutor {
     using ECDSA for bytes32;
     using SafeERC20 for IERC20;
+    using Address for address;
 
     address public immutable claimsManager;
     address public immutable offerSigner;
@@ -27,31 +29,19 @@ contract PolicyOfferExecutor {
     }
 
     function executeOffer(
-        address claimant,
-        uint224 coverageAmountInUsd,
-        uint32 claimsAllowedFrom,
-        uint32 claimsAllowedUntil,
-        string calldata policy,
+        bytes calldata policyData,
         uint256 offerAmount,
         uint256 offerExpiration,
         bytes calldata offerSignature
     ) external returns (bytes32 policyHash) {
         require(block.timestamp < offerExpiration, "Offer expired");
-        IERC20(token).safeTransferFrom(msg.sender, beneficiary, offerAmount);
-        policyHash = IClaimsManager(claimsManager).createPolicy(
-            claimant,
-            coverageAmountInUsd,
-            claimsAllowedFrom,
-            claimsAllowedUntil,
-            policy
-        );
         require(
             (
                 keccak256(
-                    abi.encodePacked(
+                    abi.encode(
                         block.chainid,
                         address(this),
-                        policyHash,
+                        policyData,
                         offerAmount,
                         offerExpiration
                     )
@@ -59,5 +49,48 @@ contract PolicyOfferExecutor {
             ).recover(offerSignature) == offerSigner,
             "Signature mismatch"
         );
+        IERC20(token).safeTransferFrom(msg.sender, beneficiary, offerAmount);
+        policyHash = abi.decode(
+            claimsManager.functionCall(
+                abi.encodePacked(
+                    IClaimsManager.createPolicy.selector,
+                    policyData
+                )
+            ),
+            (bytes32)
+        );
+    }
+
+    function executeOffer(
+        bytes[] calldata policyData,
+        uint256 offerAmount,
+        uint256 offerExpiration,
+        bytes calldata offerSignature
+    ) external {
+        require(block.timestamp < offerExpiration, "Offer expired");
+        require(
+            (
+                keccak256(
+                    abi.encode(
+                        block.chainid,
+                        address(this),
+                        policyData,
+                        offerAmount,
+                        offerExpiration
+                    )
+                ).toEthSignedMessageHash()
+            ).recover(offerSignature) == offerSigner,
+            "Signature mismatch"
+        );
+        IERC20(token).safeTransferFrom(msg.sender, beneficiary, offerAmount);
+        uint256 countPolicy = policyData.length;
+        for (uint256 indPolicy = 0; indPolicy < countPolicy; indPolicy++) {
+            claimsManager.functionCall(
+                abi.encodePacked(
+                    IClaimsManager.createPolicy.selector,
+                    policyData[indPolicy]
+                )
+            );
+        }
     }
 }
